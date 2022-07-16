053469798fcbbc41308036134b383772df529d51
leocfong <38301700+leocfong@users.noreply.github.com>
2022-07-16 10:23:16 +0930

    double digest started

--- bio/main.cpp
+++ bio/main.cpp
@@ -6,8 +6,7 @@
 #include "digest.h"
 #include "Util.h"
 using namespace std;
-
-int main()
+void PartialDigest()
 {
     vector<int> L{ 2, 2, 3, 3, 4, 5, 6, 7, 8, 10 };
     vector<int> X; vector<int> Answer;
@@ -21,5 +20,52 @@ int main()
     sort(D.begin(), D.end());
     cout << "Reconstructing partial digest" << endl;
     Render(D); cout << endl;
+}
+void DoubleDigest()
+{
+    vector<int> a{ 1,4,5,6,2 };
+    vector<int> b{ 3,8,3,3, 1 };
+    vector<int> ab{ 1,2,2,5,1,3,2,1,1};
+    int a_size = a.size();
+    int b_size = b.size();
+    int ab_size = ab.size();
+    cout <<"a size=" << a_size << ", a="; Render(a); cout << endl;
+    cout << "b size=" << b_size << ", b="; Render(b); cout << endl;
+    cout << "ab size=" << ab_size << ", ab="; Render(ab); cout << endl;
+
+    int a_index = 0;
+    int b_index = 0;
+    vector<int> f;
+    bool cont = true; int ca = 0; int cb = 0;
+    while (cont)
+    {
+        ca = ca + a[a_index];
+        cb = cb + b[b_index];
+        if (cb > ca)
+        {
+            int g = (cb - ca);
+            f.push_back(g);
+        }
+        else
+        {
+            int g = (ca - cb);
+            f.push_back(g);
+        }
+        if ((a_index< a_size) && (cb > ca)) {
+            a_index++;
+        }
+        else if (b_index < b_size){
+            b_index++;
+        }
+        else{
+            cont = false;
+        }
+    }
+    cout << "f:";
+    Render(f); cout << endl;
+}
+int main()
+{
+    PartialDigest();
     return 0;
 }
